cb5ef24a432f1a03c38ee2a343b17b8940e538d3
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-01-27 16:30:29 +0200

    11th lecture

---
+++ 11. Lab - Subqueries and JOINs/02-towns-and-addresses.sql
@@ -0,0 +1,8 @@
+SELECT 
+	a.town_id,
+    t.name,
+    a.address_text
+FROM addresses AS a
+	JOIN towns AS t ON a.town_id = t.town_id AND 
+		t.name IN ('San Francisco', 'Sofia','Carnation')
+ORDER BY a.town_id, a.address_id;
